2234c93127ca1207b830161ee53058f525bed0de
Adam Stirtan <adam.stirtan@fernsoftware.com>
2019-10-02 12:43:27 -0400

    Combined bot and handler

--- Hermes.Core/HermesBot.cs
+++ Hermes.Core/HermesBot.cs
@@ -1,10 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Reflection;
+using System.Threading.Tasks;
 
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 
-using Hermes.Core.Commands;
 using Hermes.Core.Configuration;
 
 namespace Hermes.Core
@@ -13,23 +13,21 @@ namespace Hermes.Core
     {
         private readonly IBotConfiguration _botConfiguration;
         private readonly DiscordSocketClient _client;
-
-        private CommandHandler _handler;
+        private readonly CommandService _commands;
 
         public HermesBot(IBotConfiguration botConfiguration)
         {
             _botConfiguration = botConfiguration;
 
             _client = new DiscordSocketClient();
+            _commands = new CommandService();
         }
 
         public async Task StartAsync()
         {
-            var commandService = new CommandService();
+            _client.MessageReceived += HandleCommandAsync;
 
-            _handler = new CommandHandler(_client, commandService);
-
-            await _handler.InstallCommandsAsync();
+            await _commands.AddModulesAsync(assembly: Assembly.GetExecutingAssembly(), services: null);
 
             await _client.LoginAsync(TokenType.Bot, _botConfiguration.Credentials.Token);
             await _client.StartAsync();
@@ -42,6 +40,30 @@ namespace Hermes.Core
                 await _client.LogoutAsync();
                 await _client.StopAsync();
             }
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            if (!(arg is SocketUserMessage message))
+            {
+                return;
+            }
+
+            int argPos = 0;
+
+            if (!(message.HasCharPrefix('!', ref argPos) ||
+                message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+                message.Author.IsBot)
+            {
+                return;
+            }
+
+            var context = new SocketCommandContext(_client, message);
+
+            await _commands.ExecuteAsync(
+                context: context,
+                argPos: argPos,
+                services: null);
         }
     }
 }
