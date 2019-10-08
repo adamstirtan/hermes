fdedd5b64e5019cc3fe34444b6ad2118a13792d9
Adam Stirtan <adam.stirtan@fernsoftware.com>
2019-10-08 08:43:07 -0400

    Creating talk module

---
+++ Hermes.Core/Modules/TalkModule/TalkModule.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+using Discord.Commands;
+
+namespace Hermes.Core.Modules.TalkModule
+{
+    public class TalkModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("talk")]
+        [Summary("Synthesis speech using Markov model")]
+        public async Task TalkAsync()
+        {
+            await ReplyAsync("I'm still learning about you");
+        }
+    }
+}
