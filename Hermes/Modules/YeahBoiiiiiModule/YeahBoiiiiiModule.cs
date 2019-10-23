8743ffa19ad069f24c814a0aad7d278fa75022d2
Adam Stirtan <adam.stirtan@fernsoftware.com>
2019-10-23 20:02:20 +0530

    Added !yb trigger

---
+++ Hermes/Modules/YeahBoiiiiiModule/YeahBoiiiiiModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using System.Threading.Tasks;
+
+using Discord.Commands;
+
+namespace Hermes.Modules.YeahBoiiiiiModule
+{
+    public class YeahBoiiiiiModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("yb")]
+        [Summary("Yeah boooiiiiiiiiii")]
+        public async Task YeahBoiiiiiAsync()
+        {
+            var yeahBoi = "yeah boy";
+            var random = new Random(DateTime.UtcNow.Millisecond);
+
+            for (var i = 0; i < random.Next(10, 30); i++)
+            {
+                yeahBoi += "-oi";
+            }
+
+            await ReplyAsync(yeahBoi, true);
+        }
+    }
+}
